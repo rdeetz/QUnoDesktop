df4a483f0bf5dcb38f1556acf43909d052a48459
Roger Deetz <rdeetz@gmail.com>
2021-01-11 21:38:31 -0500

    Fixed the namespace and now we good.

--- QUnoWindows/Properties/Resources.Designer.cs
+++ QUnoWindows/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Mooville.QUno.Windows.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mooville.QUno.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mooville.QUno.Windows.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
